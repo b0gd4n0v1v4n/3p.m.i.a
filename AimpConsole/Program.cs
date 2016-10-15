95c56de9e0ff84aaecbb8e1be8a619bfe85798dd
Strh-srv <Strh-srv@STRH-SRVPK>
2016-10-15 17:41:21 +0400

    йцу

--- AimpConsole/Program.cs
+++ AimpConsole/Program.cs
@@ -28,6 +28,8 @@ namespace AimpConsole
                 catch(AuthorizationException)
                 {
                     Console.WriteLine("Сервис запущен...");
+
+
                 }
                 catch(Exception ex)
                 {
